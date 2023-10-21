0fb634babceeac39c5d0b852c9d37ea2d1ab6cc1
Elba <elba.duishekeeva16@gmail.com>
2023-10-21 18:50:10 +0600

    media

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const mobileBtn = document.querySelector('.drop')
+const mobileMenu = document.querySelector('.mobile-menu')
+const header = document.querySelector('.header')
+const body = document.querySelector('body')
+
+mobileBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('show')
+    header.classList.add('static')
+    body.style.overflow = 'hidden'
+})
+
+mobileMenu.addEventListener('click', (e) => {
+    mobileMenu.classList.remove('show')
+    header.classList.remove('static')
+    body.style.overflow = 'unset'
+})
